8924bdcd2b7e96c75d88ab5cbf0c5fcee580f12b
ajnatzic <natzica@mail.gvsu.edu>
2019-08-13 08:59:06 -0400

    refactor: change if statements to switch statement
    Also, add default case to switch statement

--- bot.js
+++ bot.js
@@ -119,156 +119,163 @@ client.on("message", async message => {
 	"default": "Yeehaw",
 	"joined [@user]": "Use this command in conjunction with an [@user] to see when a specific user joined the server. Or just use the command by itself to see when you joined.",
 	"botcheck [@user]": "Use this command to see if a user has the 'bot' role. Use the command by itself to see if you have the bot role.",
-	"kick [@user] [reason]": "Use this command to kick someone. You must be one of the biggest boys to do this.",
-	"ban [@user] [reason]": "Use this command to ban someone. You must be one of the biggest boys to do this.",
+	"kick [@user] [kickReason]": "Use this command to kick someone. You must be one of the biggest boys to do this.",
+	"ban [@user] [banReason]": "Use this command to ban someone. You must be one of the biggest boys to do this.",
 	"purge [number]": "Delete between 2 and 500 messages in a channel. You must be a big boy to do this."
   }
 
-  // Gives the user a list of commands as well as a description on how to use them
-  if(command === "help"){
-		  var menu = (`Help menu
-		  --------------------------------------------------------------------\n`);
-	  for(var key in commandsList) {
-		  menu += (`?${key} | ${commandsList[key]}\n`);
-	  }
-	  message.channel.send(menu);
-  }
+  // Var used to store mentioned member IDs from a message
+  let member;
+  switch(command){
+    // Gives the user a list of commands as well as a description on how to use them
+    case "help":
+      var menu = (`Help menu
+        --------------------------------------------------------------------\n`);
+      for(var key in commandsList) {
+        menu += (`?${key} | ${commandsList[key]}\n`);
+      }
+      message.channel.send(menu);
+    break;
 
-  // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-  // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-  if(command === "ping") {
-    const m = await message.channel.send("Calculating ping...");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    case "ping":
+      const m = await message.channel.send("Calculating ping...");
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    break;
 
-  // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-  // To get the "message" itself we join the `args` back into a string with spaces:
-  if(command === "say") {
-    const sayMessage = args.join(" ");
-    // Then we delete the command message.
-    message.delete().catch(O_o=>{});
-    // And we get the bot to say something:
-    message.channel.send(sayMessage);
-  }
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
+    case "say":
+      const sayMessage = args.join(" ");
+      // Then we delete the command message.
+      message.delete().catch(O_o=>{});
+      // And we get the bot to say something:
+      message.channel.send(sayMessage);
+    break;
 
-  // Returns the amount of users in the server
-  if(command === "usercount"){
-	message.channel.send(`There are currently ${client.users.size} members in this server!`);
-  }
+    // Returns the amount of users in the server
+    case "usercount":
+      message.channel.send(`There are currently ${client.users.size} members in this server!`);
+    break;
 
-  // Returns the amount of messages in a specific channel
-  if(command === "msgcount") {
-	message.channel.fetchMessages({limit: 10000})	//FIXME can only count to 100 messages
-  		.then(messages => message.channel.send(`There are currently ${messages.size} messages in this server!`))
-  		.catch(console.error);
-  }
+    // Returns the amount of messages in a specific channel
+    case "msgcount":
+      message.channel.fetchMessages({limit: 10000})	//FIXME can only count to 100 messages
+          .then(messages => message.channel.send(`There are currently ${messages.size} messages in this server!`))
+          .catch(console.error);
+    break;
 
-  // Returns the link to a default dance gif, yeehaw
-  if(command === "default"){
-	  message.channel.send({files: ["https://cdn.discordapp.com/attachments/553401247948996608/566611034090110977/a3229cf.gif"]});
-  }
+    // Returns the link to a default dance gif, yeehaw
+    case "default": // Note: NOT the default case, the command is literally called default
+      message.channel.send({files: ["https://cdn.discordapp.com/attachments/553401247948996608/566611034090110977/a3229cf.gif"]});
+    break;
 
-  // Tells the user when a person joined
-  if(command === "joined"){
-	const member = message.mentions.members.first();
-	
-	// If only "!joined" is typed with no user, or user is invalid
-	if(!member) {
-		const author = message.member;
-		message.channel.send(`${author} joined the server on ${author.joinedAt}`);
-	}
-	// Otherwise, find the user inputted
-	else {
-		message.channel.send(`${member} joined the server on ${member.joinedAt}`);
-	}
-  }
+    // Tells the user when a person joined
+    case "joined":
+      member = message.mentions.members.first();
+      
+      // If only "!joined" is typed with no user, or user is invalid
+      if(!member) {
+        const author = message.member;
+        message.channel.send(`${author} joined the server on ${author.joinedAt}`);
+      }
+      // Otherwise, find the user inputted
+      else {
+        message.channel.send(`${member} joined the server on ${member.joinedAt}`);
+      }
+    break;
 
-  // Tells the user if the mentioned member is a bot
-  // Only checks if user has the "Bots" role. Not if it's actually a bot.
-  if(command === "botcheck"){
-	const user = message.mentions.members.first();
-	const author = message.author;
-	if(!user) {
-		if(message.member.roles.some(r=>["Bots"].includes(r.name))) {
-		message.channel.send(`${author} is a bot!`);
-		}
-		else {
-			message.channel.send(`${author} is not a bot!`);
-		}
-	}
-	else {
-		if(user.roles.some(r=>["Bots"].includes(r.name))) {
-		message.channel.send(`${user} is a bot!`);
-		}
-		else {
-			message.channel.send(`${user} is not a bot!`);
-		}
-	}
-  }
+    // Tells the user if the mentioned member is a bot
+    // Only checks if user has the "Bots" role. Not if it's actually a bot.
+    case "botcheck":
+      const user = message.mentions.members.first();
+      const author = message.author;
+      if(!user) {
+        if(message.member.roles.some(r=>["Bots"].includes(r.name))) {
+        message.channel.send(`${author} is a bot!`);
+        }
+        else {
+          message.channel.send(`${author} is not a bot!`);
+        }
+      }
+      else {
+        if(user.roles.some(r=>["Bots"].includes(r.name))) {
+        message.channel.send(`${user} is a bot!`);
+        }
+        else {
+          message.channel.send(`${user} is not a bot!`);
+        }
+      }
+    break;
 
-  if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit:
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["The Biggest Boys"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+    case "kick":
+      // This command must be limited to mods and admins. In this example we just hardcode the role names.
+      // Please read on Array.some() to understand this bit:
+      // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+      if(!message.member.roles.some(r=>["The Biggest Boys"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
 
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.server.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable)
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      // Let's first check if we have a member and if we can kick them!
+      // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+      // We can also support getting the member by ID, which would be args[0]
+      member = message.mentions.members.first() || message.server.members.get(args[0]);
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.kickable)
+        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
 
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+      // slice(1) removes the first part, which here should be the user mention or ID
+      // join(' ') takes all the various parts to make it a single string.
+      let kickReason = args.slice(1).join(' ');
+      if(!kickReason) kickReason = "No kick reason provided";
 
-    // Attempt to kick the member, throw error if not possible
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+      // Attempt to kick the member, throw error if not possible
+      await member.kick(kickReason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${kickReason}`);
 
-  }
+    break;
 
-  // Most of this command is identical to kick, except that here we'll only let admins do it.
-  if(command === "ban") {
-    if(!message.member.roles.some(r=>["The Biggest Boys"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    case "ban":
+      if(!message.member.roles.some(r=>["The Biggest Boys"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
 
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable)
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+      member = message.mentions.members.first();
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.bannable)
+        return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
 
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+      let banReason = args.slice(1).join(' ');
+      if(!banReason) banReason = "No ban reason provided";
 
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
+      await member.ban(banReason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+      message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${banReason}`);
+    break;
 
-  // This command removes all messages from all users in the channel, up to 500.
-  if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 500.
+    case "purge":
+      if(!message.member.roles.some(r=>["Big Boys"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
+        
+        // get the delete count, as an actual number.
+        const deleteCount = parseInt(args[0], 10);
 
-	if(!message.member.roles.some(r=>["Big Boys"].includes(r.name)) )
-	  return message.reply("Sorry, you don't have permissions to use this!");
-	  
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
+        if(!deleteCount || deleteCount < 2 || deleteCount > 500)
+          return message.reply("Please provide a number between 2 and 500 for the number of messages to delete");
 
-    if(!deleteCount || deleteCount < 2 || deleteCount > 500)
-      return message.reply("Please provide a number between 2 and 500 for the number of messages to delete");
+        // So we get our messages, and delete them. Simple enough, right?
+        const fetched = await message.channel.fetchMessages({limit: deleteCount});
+        message.channel.bulkDelete(fetched)
+          .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    break;
 
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    // If the command is not recognized
+    default:
+      return message.channel.send("Honestly, I have no idea what that means. Use \"?help\" to see the list of commands.");
   }
 });
 

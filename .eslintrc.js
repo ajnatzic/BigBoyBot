54f6185a5bafb07ebbf5d22a1e13a6d439f5f21d
ajnatzic <natzica@mail.gvsu.edu>
2019-08-13 15:20:00 -0400

    style: adhere to eslint style

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,23 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    'linebreak-style': ['error', 'unix'],
+        'brace-style': ['error', '1tbs'],
+        'comma-dangle': ['error', 'always-multiline'],
+        'curly': 'error',
+        'eqeqeq': 'error',
+        'indent': [
+            'error',
+            4
+        ],
+        'prefer-const': 'error',
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
   }
 }

22b99a10ea49a112fbea818496e860ef105dc6da
AJ Natzic <42810761+ajnatzic@users.noreply.github.com>
2019-09-26 18:54:24 -0400

    feat: chad replies to 'just' with 4head

--- bot.js
+++ bot.js
@@ -106,6 +106,11 @@ client.on('message', async message => {
             const emoji = message.guild.emojis.find(emoji => emoji.name === 'chad');
             message.react(emoji);
         }
+        // Check if the message includes 'just'
+        if (new RegExp("\\b" + 'just' + "\\b").test(msgText)) {
+          const emoji = message.guild.emojis.find(emoji => emoji.name === '4Head');
+          message.channel.send(`${emoji}`);
+        }
         // Check if the message includes 'aj'
         if (msgText.includes('aj') || msgText.includes(userMention)) {
             // An array containing the list of emojis related to aj, change as emojis are added
